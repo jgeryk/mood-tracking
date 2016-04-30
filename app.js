3a3f4002f5aa946451a9f08faf75aef7b538bc13
Jacob Geryk <jacobgeryk@gmail.com>
2016-04-30 11:46:49 -0400

    adding routeprovider to app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 var app = angular.module('mood-tracker', []);
+  .config(function($routeProvider, $locationProvider){
+    .when('/', {
+           templateUrl : './templates/landing.html',
+           controller: 'landingController'
+       })
+       .when('/setmood', {
+           templateUrl : './templates/plutchik.html',
+           controller: 'formController'
+       })
+       .otherwise({
+           redirectTo: '/'
+       });
+  })
