3c70d15ea73474ef6cd76d0500bc124751e5922d
Kevin Chan <kcchan@umass.edu>
2016-05-02 00:35:10 -0400

    You can select on the calendars

--- public/js/controllers/calendarController.js
+++ public/js/controllers/calendarController.js
@@ -2,6 +2,7 @@ angular.module('mood-tracker')
   .controller('calendarController', function($scope, $location, $timeout, $interval) {
 
     var overflow = document.getElementById('overflow_container');
+
     var position = 20;
     var initial;
 
@@ -11,21 +12,53 @@ angular.module('mood-tracker')
       };
     }, 5);
 
-    overflow.addEventListener('touchstart', function(event) {
-      initial = event.targetTouches[0].clientY;
-    }, false);
+    if (overflow !== null) {
+      overflow.addEventListener('touchstart', function(event) {
+        initial = event.targetTouches[0].clientY;
+      }, false);
 
-    overflow.addEventListener('touchmove', function(event) {
-      var touch = event.targetTouches[0];
-      position -= initial-touch.clientY;
-      initial = touch.clientY;
-      var maxH = -window.innerHeight * 0.31;
-      if (position > 0) position = 20;
-      if (position < maxH) position = maxH;
+      overflow.addEventListener('touchmove', function(event) {
+        var touch = event.targetTouches[0];
+        position -= initial-touch.clientY;
+        initial = touch.clientY;
+        var maxH = -window.innerHeight * 0.31;
+        if (position > 0) position = 20;
+        if (position < maxH) position = maxH;
     
-    }, false);
+      }, false);
+    }
 
     $scope.go = function () {
       $location.path('/');
     }
+
+    $scope.expand = function (event) {
+      //$scope.hide = {
+        //'display' : 'none'
+      //}
+      //console.log(event);
+    }
+    
+    $(document).ready(function() {
+      $('img').click(function(e) {
+        var offset = $(this).offset();
+        var height = $(this).context.clientHeight;
+        var width = $(this).context.clientWidth;
+
+        console.log($(this));
+        // Get the offset relative to the image
+        var vNum = Math.floor((e.pageY-offset.top) / (height/7));
+        var vOff = height/7.8 + (vNum-1)*height/6.9;
+        var hNum = Math.floor((e.pageX-offset.left) / (width/7));
+
+        $scope.highlight = {
+          'display': 'block',
+          'width': width/7+2,
+          'height': height/7+3,
+          'top': offset.top + vOff,
+          'left': offset.left + width/7*hNum,
+          'z-index' : 1
+        }
+      });
+    });
   });
